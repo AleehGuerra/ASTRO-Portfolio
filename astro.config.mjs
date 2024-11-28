86c0ae6e563323e33862a61ebf97b009e6121226
Alejandro Guerra Rubio <aleehguerra@gmail.com>
2024-11-27 22:54:48 -0500

    Vista Movil

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+  }, 
   integrations: [tailwind()]
 });
